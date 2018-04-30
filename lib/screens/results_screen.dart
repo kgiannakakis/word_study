eaed75c5829e89ea78e636664ee0c09baa9609f6
Kostas Giannakakis <kgiannak1974@gmail.com>
2018-04-30 15:46:36 +0300

    Results screen

---
+++ lib/screens/results_screen.dart
@@ -0,0 +1,62 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:word_study/screens/home_screen.dart';
+
+class ResultsScreen extends StatelessWidget {
+
+  final List<charts.Series<QuizResults, int>> seriesList = [
+    new charts.Series<QuizResults, int>(
+      id: 'Results',
+      domainFn: (QuizResults results, _) => results.category,
+      measureFn: (QuizResults results, _) => results.count,
+      //colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
+      //colorFn: (QuizResults results, _) => results.category == 1 ?
+      //  charts.Color.fromHex(code: '0xFF00C853') : charts.Color.fromHex(code: '0xFFFF1744'),
+      data: <QuizResults>[ new QuizResults(0, 10), new QuizResults(1, 20)],
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Results'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.close),
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                new MaterialPageRoute(builder: (BuildContext context) => new HomeScreen()),
+                    (route) => false,
+              );
+            },
+          )
+        ]
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new charts.PieChart<QuizResults, int>(seriesList,
+            animate: true,
+            defaultRenderer: new charts.ArcRendererConfig(arcWidth: 60)
+          ),
+          new Center(child: new Text('23%')),
+          new Padding(
+              padding: EdgeInsets.all(10.0),
+              child: new Align(
+                alignment: Alignment.topCenter,
+                child: new Text('You got 10 out of 20 right!'),
+              )
+          ),
+          //new Text('You got 20 out of 30 correct!')
+        ],
+      )
+    );
+  }
+}
+
+class QuizResults {
+  final int category;
+  final int count;
+
+  QuizResults(this.category, this.count);
+}
